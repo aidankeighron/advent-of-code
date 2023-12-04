7871c52a3869647f33c76b7d2741d4f4dd415822
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2023-12-04 00:44:50 -0500

    part 2

--- day4.py
+++ day4.py
@@ -5,18 +5,42 @@ array = load_day(4)
 total = 0
 part_2 = False
 
-for line in array:
-    numbers, winning = line.split("|")
-    winning = winning.split(" ")
-    numbers = numbers.split(":")[1].split(" ")
-    score = 0
+if part_2:
+    number_of_copies = []
+    for line in array:
+        numbers, winning = line.split("|")
+        winning = winning.split(" ")
+        numbers = numbers.split(":")[1].split(" ")
+        
+        copies = 0
+        for num in numbers:
+            if num != '' and num in winning:
+                copies += 1
+        number_of_copies.append(copies)
 
-    for num in numbers:
-        if num != '' and num in winning:
-            if score == 0:
-                score = 1
-            else:
-                score *= 2
-    total += score
+    new_array = [1]*len(number_of_copies)
+    for i, copies in enumerate(number_of_copies):
+        for copy in range(copies):
+            try:
+                new_array[i+copy+1] += 1*new_array[i]
+            except:
+                ...
 
-result(total)
+    result(sum(new_array))
+else:
+    for line in array:
+        numbers, winning = line.split("|")
+        winning = winning.split(" ")
+        numbers = numbers.split(":")[1].split(" ")
+        score = 0
+        
+        for num in numbers:
+            if num != '' and num in winning:
+                if score == 0:
+                    score = 1
+                else:
+                    score *= 2
+        total += score
+
+    result(total)
+
