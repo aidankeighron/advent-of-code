69258f539e707d8cbe29dd8c9841d701031a171f
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2023-12-04 00:15:06 -0500

    part 1

--- day4.py
+++ day4.py
@@ -5,4 +5,18 @@ array = load_day(4)
 total = 0
 part_2 = False
 
+for line in array:
+    numbers, winning = line.split("|")
+    winning = winning.split(" ")
+    numbers = numbers.split(":")[1].split(" ")
+    score = 0
+
+    for num in numbers:
+        if num != '' and num in winning:
+            if score == 0:
+                score = 1
+            else:
+                score *= 2
+    total += score
+
 result(total)
