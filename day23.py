66215458860152d2b0bc63c89149f88b80faf0b3
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2023-12-24 12:14:42 -0600

    day 22

---
+++ day23.py
@@ -0,0 +1,13 @@
+from util import init, load_day, load_file, result
+import math
+import numpy as np
+init()
+
+array = load_day(23)
+part_2 = False
+total = 0
+
+for line in array:
+    
+
+result(total)
