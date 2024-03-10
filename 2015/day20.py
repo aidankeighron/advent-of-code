9663bd2bd04b7d08ae95825aa952a7bfd5f95904
aidankeighron <aidankeighron@gmail.com>
2024-03-09 23:03:48 -0500

    day 20

--- 2015/day20.py
+++ 2015/day20.py
@@ -8,18 +8,23 @@ array = 33100000
 part_2 = False
 total = 0
 
-from functools import reduce
+import math
 
-def factors(n):    
-    return sum(set(reduce(list.__add__,([i*10, (n//i)*10] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
+def get_divisors(n):
+    small_divisors = [i for i in range(1, int(math.sqrt(n)) + 1) if n % i == 0]
+    large_divisors = [n / d for d in small_divisors if n != d * d]
+    return small_divisors + large_divisors
 
-# print(factors(4))
-# quit()
-house = 0
-while True:
-    house += 1
-    total = factors(house)
-    
-    if total == array:
-        result(total)
-        break
+target = 33100000
+part_one, part_two = None, None
+i = 0
+while not part_one or not part_two:
+    i += 1
+    divisors = get_divisors(i)
+    if not part_one:
+        if sum(divisors) * 10 >= target:
+            part_one = i
+    if not part_two:
+        if sum(d for d in divisors if i / d <= 50) * 11 >= target:
+            part_two = i
+print(part_one, part_two)
